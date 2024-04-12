281dc4dba3cdd50fc0105930e12dd08318d06d7c
unknown <slee230@uncc.edu>
2024-04-11 21:11:05 -0400

    feat: add customer schema

---
+++ api/schemas/customers.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+
+class CustomerBase(BaseModel):
+    name: str
+    email: str
+    phone_number: str
+    address: str
+
+class CustomerCreate(CustomerBase):
+    pass
+
+class CustomerUpdate(BaseModel):
+    name: Optional[str] = None
+    email: Optional[str] = None
+    phone_number: Optional[str] = None
+    address: Optional[str] = None
+
+class Customer(CustomerBase):
+    id: int
+
+    class ConfigDict:
+        from_attributes = True
