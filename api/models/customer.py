9800d9f2bdf2768620a3c2ed444c1cfcfe3529b1
Txuj Yang <tyang37@uncc.edu>
2024-04-16 12:37:24 -0400

    fix: updated customer table

--- api/models/customer.py
+++ api/models/customer.py
@@ -7,7 +7,7 @@ class Customer(Base):
     __tablename__ = "customers"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name = Column(String, nullable=False)
-    email = Column(String, unique=True, nullable=False)
-    phone_number = Column(String, unique=True, nullable=False)
-    address = Column(String, nullable=False)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    phone_number = Column(String(100), unique=True, nullable=False)
+    address = Column(String(100), nullable=False)
