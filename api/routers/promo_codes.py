3d242eebd55e9a439b372c86ea8e52b4ec3ba2d7
Saafa Shahriar <sshahri1@uncc.edu>
2024-04-27 22:28:03 -0400

    feat: added promo codes router

--- api/routers/promo_codes.py
+++ api/routers/promo_codes.py
@@ -1,2 +1,35 @@
-def router():
-    return None
+from fastapi import APIRouter, Depends, FastAPI, status, Response
+from sqlalchemy.orm import Session
+from ..controllers import promo_codes as controller
+from ..schemas import promo_codes as schema
+from ..dependencies.database import engine, get_db
+
+router = APIRouter(
+    tags=['Promo Codes'],
+    prefix="/promocodes"
+)
+
+
+@router.post("/", response_model=schema.PromoCode)
+def create(request: schema.PromoCodesCreate, db: Session = Depends(get_db)):
+    return controller.create(db=db, request=request)
+
+
+@router.get("/", response_model=list[schema.PromoCode])
+def read_all(db: Session = Depends(get_db)):
+    return controller.read_all(db)
+
+
+@router.get("/{item_id}", response_model=schema.PromoCode)
+def read_one(item_id: int, db: Session = Depends(get_db)):
+    return controller.read_one(db, item_id=item_id)
+
+
+@router.put("/{item_id}", response_model=schema.PromoCode)
+def update(item_id: int, request: schema.PromoCode, db: Session = Depends(get_db)):
+    return controller.update(db=db, request=request, item_id=item_id)
+
+
+@router.delete("/{item_id}")
+def delete(item_id: int, db: Session = Depends(get_db)):
+    return controller.delete(db=db, item_id=item_id)
