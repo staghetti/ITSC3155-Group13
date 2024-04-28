01fb104bd767f34d69eb5e2dbe76213f2db5876b
Saafa Shahriar <sshahri1@uncc.edu>
2024-04-27 22:34:50 -0400

    fix: implemented promo codes router

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,7 +1,8 @@
-from . import orders, order_details, payments
+from . import orders, order_details, payments, promo_codes
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(order_details.router)
     app.include_router(payments.router)
+    app.include_router(promo_codes.router)
