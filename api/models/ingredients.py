91e8415cf433b6e7d9bd208f56be4dbb2b176138
Saul <saulavi21@gmail.com>
2024-04-28 21:19:37 -0400

    fix: implement order placement as guest

--- api/models/ingredients.py
+++ api/models/ingredients.py
@@ -4,13 +4,11 @@ from datetime import datetime
 from ..dependencies.database import Base
 
 
-class Recipe(Base):
-    __tablename__ = "recipes"
+class Resource(Base):
+    __tablename__ = "resources"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    sandwich_id = Column(Integer, ForeignKey("sandwiches.id"))
-    resource_id = Column(Integer, ForeignKey("resources.id"))
+    item = Column(String(100), unique=True, nullable=False)
     amount = Column(Integer, index=True, nullable=False, server_default='0.0')
 
-    sandwich = relationship("Sandwich", back_populates="recipes")
-    resource = relationship("Resource", back_populates="recipes")
+    menu_items = relationship("MenuItems", back_populates="resource")
