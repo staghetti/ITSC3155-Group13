209baa0637fe5732e2b3088513ac9c3deb5f38e6
Saul <saulavi21@gmail.com>
2024-04-28 21:35:08 -0400

    fix: updated schema to handle order placement as guest

--- api/schemas/orders.py
+++ api/schemas/orders.py
@@ -4,10 +4,12 @@ from pydantic import BaseModel
 from .order_details import OrderDetail
 
 
-
 class OrderBase(BaseModel):
-    customer_name: str
+    # customer_name: str
+    customer_name: Optional[str] = None
+    customer_id: Optional[int] = None  # Allows null for guest orders
     description: Optional[str] = None
+    is_guest: bool = True  # Indicates if the order is placed by a guest
 
 
 class OrderCreate(OrderBase):
@@ -15,8 +17,9 @@ class OrderCreate(OrderBase):
 
 
 class OrderUpdate(BaseModel):
-    customer_name: Optional[str] = None
+    customer_id: Optional[int] = None
     description: Optional[str] = None
+    is_guest: Optional[bool] = None  # Allow updating guest status if needed
 
 
 class Order(OrderBase):
