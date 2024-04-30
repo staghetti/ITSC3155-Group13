9603ff42266aee009123f9c2bf0f3aaf6202feb8
Saul <saulavi21@gmail.com>
2024-04-29 20:21:54 -0400

    feat: replace resources model with ingredients model

---
+++ api/models/ingredients.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from ..dependencies.database import Base
+
+
+class Ingredients(Base):
+    __tablename__ = "ingredients"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), unique=True, nullable=False)
+    amount = Column(Integer, nullable=False)
+
