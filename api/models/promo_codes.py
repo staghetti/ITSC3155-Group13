e1a6fc61fb2003ffcab2bd26320caadc45c953b6
codie <codiedautorio03@gmail.com>
2024-04-30 17:23:28 -0400

    feat: updated promo_code column

--- api/models/promo_codes.py
+++ api/models/promo_codes.py
@@ -8,7 +8,7 @@ class PromoCode(Base):
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     order_id = Column(Integer, ForeignKey("orders.id"))
-    promo_code = Column(String(300), unique=True, nullable=False)
+    promo_code = Column(String, unique=True, nullable=False)
     discount_amount = Column(DECIMAL, nullable=False)
     start_date = Column(DATETIME, default= datetime.now(),nullable=False)
     end_date = Column(DATETIME, default= datetime, nullable=False)
