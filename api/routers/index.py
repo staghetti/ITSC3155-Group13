6810fdbf64b560434cb86244dd8b406509b566a7
Saul <saulavi21@gmail.com>
2024-04-30 16:35:00 -0400

    fix: add recipes, categories, and menu_items router

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, payments, promo_codes, customers, ingredients
+from . import orders, order_details, payments, promo_codes, customers, menu_items, categories, ingredients, recipes
 
 
 def load_routes(app):
@@ -7,4 +7,8 @@ def load_routes(app):
     app.include_router(payments.router)
     app.include_router(promo_codes.router)
     app.include_router(customers.router)
+    app.include_router(menu_items.router)
+    app.include_router(categories.router)
     app.include_router(ingredients.router)
+    app.include_router(recipes.router)
+
