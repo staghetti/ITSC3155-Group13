590e5d618d53124810f684c0fb83791ccd74c993
Saul <saulavi21@gmail.com>
2024-04-30 17:10:26 -0400

    feat(order_details): implement inventory management and alert system in model

--- api/models/order_details.py
+++ api/models/order_details.py
@@ -3,13 +3,14 @@ from sqlalchemy.orm import relationship
 from datetime import datetime
 from ..dependencies.database import Base
 
+
 class OrderDetail(Base):
     __tablename__ = "order_details"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     order_id = Column(Integer, ForeignKey("orders.id"))
-    sandwich_id = Column(Integer, ForeignKey("sandwiches.id"))
+    menu_item_id = Column(Integer, ForeignKey("menu_items.id"))
     amount = Column(Integer, index=True, nullable=False)
 
-    sandwich = relationship("Sandwich", back_populates="order_details")
+    menu_item = relationship("MenuItems", back_populates="order_details")
     order = relationship("Order", back_populates="order_details")
