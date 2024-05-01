46d1232d0800eeeab355b9b4028e32d5577a538b
Saul <saulavi21@gmail.com>
2024-05-01 15:03:05 -0400

    fix: add promo code string

--- api/models/promo_codes.py
+++ api/models/promo_codes.py
@@ -3,12 +3,12 @@ from sqlalchemy.orm import relationship
 from datetime import datetime
 from ..dependencies.database import Base
 
+
 class PromoCode(Base):
-    __tablename__ = "promo_code"
+    __tablename__ = "promo_codes"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    order_id = Column(Integer, ForeignKey("orders.id"))
-    promo_code = Column(String, unique=True, nullable=False)
-    discount_amount = Column(DECIMAL, nullable=False)
-    start_date = Column(DATETIME, default= datetime.now(),nullable=False)
-    end_date = Column(DATETIME, default= datetime, nullable=False)
+    promo_code = Column(String(50), unique=True, nullable=False)
+    discount_amount = Column(DECIMAL(10, 2), nullable=False)
+    start_date = Column(DATETIME, default=datetime.now, nullable=False)
+    end_date = Column(DATETIME, default=datetime.now, nullable=False)
