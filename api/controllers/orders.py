196f65631311e9f75dbf87bd852822c681e7bf6a
Saul <saulavi21@gmail.com>
2024-04-30 21:09:05 -0400

    feat(orders-controller): implement function to find orders within a specific date range

--- api/controllers/orders.py
+++ api/controllers/orders.py
@@ -1,3 +1,4 @@
+from datetime import date
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status, Response, Depends
 from ..models import orders as model
@@ -69,3 +70,15 @@ def delete(db: Session, order_id):
         error = str(e.__dict__['orig'])
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+def get_orders_by_date_range(db: Session, start_date: date, end_date: date):
+    try:
+        query = db.query(model.Order)
+        if start_date:
+            query = query.filter(model.Order.order_date >= start_date)
+        if end_date:
+            query = query.filter(model.Order.order_date <= end_date)
+        return query.all()
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
