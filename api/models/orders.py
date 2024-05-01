c64c3d6091c40d09a39f93031f0be9b57aba92b0
Saul <saulavi21@gmail.com>
2024-05-01 14:58:29 -0400

    feat(models-orders):  add function to include promo_code to orders

--- api/models/orders.py
+++ api/models/orders.py
@@ -11,9 +11,10 @@ class Order(Base):
     customer_id = Column(Integer, ForeignKey('customers.id'), nullable=True)  # Links to a customer if not a guest
     customer_name = Column(String(100))
     description = Column(String(300))
-    order_date = Column(DATETIME, nullable=False, server_default=str(datetime.now))
+    order_date = Column(DATETIME, nullable=False, default=datetime.now)
     is_guest = Column(Boolean, default=True, nullable=False)
     order_type = Column(Enum("Takeout", "Delivery", "Dine-In", name="order_types"), default="Dine-In", nullable=False)
-
+    promo_code = Column(String(50), ForeignKey('promo_codes.promo_code'), nullable=True)  # Link to the PromoCode table
+    # Relationships
     order_details = relationship("OrderDetail", back_populates="order")
     tracking = relationship("OrderTracking", back_populates="order")
