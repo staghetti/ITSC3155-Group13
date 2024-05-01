1d0fa911e04eac8b3df72c21c99e61d397a7290c
Saul <saulavi21@gmail.com>
2024-04-30 20:39:49 -0400

    feat(models-orders): add preference for order type

--- api/models/orders.py
+++ api/models/orders.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME, Boolean, Enum
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from ..dependencies.database import Base
@@ -13,4 +13,6 @@ class Order(Base):
     description = Column(String(300))
     order_date = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
     is_guest = Column(Boolean, default=True, nullable=False)
+    order_type = Column(Enum("Takeout", "Delivery", "Dine-In", name="order_types"), default="Dine-In", nullable=False)
+
     order_details = relationship("OrderDetail", back_populates="order")
