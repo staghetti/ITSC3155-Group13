1284d9c6ae5ea961b6f3bd6b2c41a306db5829e2
Saul <saulavi21@gmail.com>
2024-05-01 16:41:10 -0400

    feat(tests-test_orders): add unit test for orders

--- api/tests/test_orders.py
+++ api/tests/test_orders.py
@@ -1,31 +1,26 @@
 from fastapi.testclient import TestClient
-from ..controllers import orders as controller
 from ..main import app
 import pytest
-from ..models import orders as model
-
-# Create a test client for the app
-client = TestClient(app)
 
 
 @pytest.fixture
-def db_session(mocker):
-    return mocker.Mock()
+def client():
+    return TestClient(app)
 
 
-def test_create_order(db_session):
-    # Create a sample order
+def test_create_order(client):
     order_data = {
-        "customer_name": "John Doe",
-        "description": "Test order"
+        "customer_name": "Customer Name Test",
+        "description": "Test order",
+        "customer_id": 1,
+        "is_guest": False,
+        "order_type": "Takeout",
+        "promo_code": "B4UEAT"
     }
 
-    order_object = model.Order(**order_data)
-
-    # Call the create function
-    created_order = controller.create(db_session, order_object)
-
-    # Assertions
-    assert created_order is not None
-    assert created_order.customer_name == "John Doe"
-    assert created_order.description == "Test order"
+    response = client.post("/orders/", json=order_data)
+    assert response.status_code == 200
+    assert response.json()['customer_name'] == "Customer Name Test"
+    assert response.json()['customer_id'] == 1
+    assert response.json()['is_guest'] is False
+    assert response.json()['order_type'] == "Takeout"
