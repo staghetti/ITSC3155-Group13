459f715ba161b6343ab3e67badea5dc5d5a40ae6
Saul <saulavi21@gmail.com>
2024-05-02 11:37:10 -0400

    feat(tests-test_orders): add unit test for order creation

--- api/tests/test_orders.py
+++ api/tests/test_orders.py
@@ -1,6 +1,6 @@
+import pytest
 from fastapi.testclient import TestClient
 from ..main import app
-import pytest
 
 
 @pytest.fixture
@@ -8,19 +8,31 @@ def client():
     return TestClient(app)
 
 
-def test_create_order(client):
+@pytest.fixture
+def create_customer(client):
+    customer_data = {
+        "name": "Jane Doe",
+        "email": "jane.doe@example.com",
+        "phone_number": "708-567-8362",
+        "address": "New Home Street"
+    }
+    response = client.post("/customers/", json=customer_data)
+    assert response.status_code == 200, "Failed to create customer"
+    return response.json()['id']
+
+
+def test_create_order(client, create_customer):
+    customer_id = create_customer
     order_data = {
-        "customer_name": "Customer Name Test",
+        "customer_name": "Jane Doe",
         "description": "Test order",
-        "customer_id": 1,
+        "customer_id": customer_id,
         "is_guest": False,
         "order_type": "Takeout",
-        "promo_code": "B4UEAT"
+        "promo_code": ""
     }
 
     response = client.post("/orders/", json=order_data)
-    assert response.status_code == 200
-    assert response.json()['customer_name'] == "Customer Name Test"
-    assert response.json()['customer_id'] == 1
-    assert response.json()['is_guest'] is False
-    assert response.json()['order_type'] == "Takeout"
+    assert response.status_code == 200, "Failed to create order"
+    assert response.json()['customer_name'] == order_data['customer_name'], "Customer name mismatch"
+    assert response.json()['order_type'] == order_data['order_type'], "Order type mismatch"
